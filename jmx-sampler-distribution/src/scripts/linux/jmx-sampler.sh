c4a20fcbbfe1386b5bec313adfe803aa8d8b07ea
Dimo Velev <dimo.velev@gmail.com>
2012-08-29 09:09:34 -0700

    Added more help info to the startup script. Added a commented out option for compatibility workaround for JMX

--- jmx-sampler-distribution/src/scripts/linux/jmx-sampler.sh
+++ jmx-sampler-distribution/src/scripts/linux/jmx-sampler.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 JAVA=java
+JAVA_OPTS=
+# Uncomment this option if you are running this application with Java >=1.6 and are connecting to a Java 1.5 JMX client.
+#JAVA_OPTS="$JAVA_OPTS -Dsun.lang.ClassLoader.allowArraySyntax=true" 
 BASEDIR=$(dirname $0)
 BASEDIR=$(readlink -f $BASEDIR/..)
 LOGCONFIG=$BASEDIR/config/logback.xml
@@ -17,7 +20,7 @@ case "$1" in
 			echo "PIDFILE $PIDFILE exists"
 			exit 1
 		fi
-		nohup $JAVA -Dlogback.configurationFile=$LOGCONFIG -cp "${BASEDIR}/lib/*" org.jmxsampler.Runner start $CONFIG &
+		nohup $JAVA -Dlogback.configurationFile=$LOGCONFIG $JAVA_OPTS -cp "${BASEDIR}/lib/*" org.jmxsampler.Runner start $CONFIG &
 		echo $! > $PIDFILE
 		echo "Started with pid $!"
 		;;
@@ -43,12 +46,21 @@ case "$1" in
 		fi
 		;;
 	check)
-		$JAVA -Dlogback.configurationFile=$LOGCONFIG_CONSOLE -cp "${BASEDIR}/lib/*" org.jmxsampler.Runner check $CONFIG
+		$JAVA -Dlogback.configurationFile=$LOGCONFIG_CONSOLE $JAVA_OPTS -cp "${BASEDIR}/lib/*" org.jmxsampler.Runner check $CONFIG
 		;;
 	metadata)
-		$JAVA -Dlogback.configurationFile=$LOGCONFIG_CONSOLE -cp "${BASEDIR}/lib/*" org.jmxsampler.Runner metadata $CONFIG
+		$JAVA -Dlogback.configurationFile=$LOGCONFIG_CONSOLE $JAVA_OPTS -cp "${BASEDIR}/lib/*" org.jmxsampler.Runner metadata $CONFIG
 		;;
 	*)
-		echo "Usage: $0 start|stop|status|check|metadata"
+		cat <<EOF
+Usage: $0 [start|stop|status|check|metadata]
+
+start     Starts the application as a daemon in the background.
+stop      Stops a running daemon (if any). This also cleans up the pid file if the application has crashed.
+status    Outputs whether the daemon is running or not.
+check     Goes through all samplers and checks whether each rule matches at least one metric. Everything is logged to STDOUT.
+metadata  Goes through all samplers and outputs the metadata of their readers. Use it to see what metrics are available and build
+          your rules based on that.
+EOF
 		;;
 esac
